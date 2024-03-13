413873ba9f8cadf9077450eaa715dcff66e0585e
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2024-03-13 22:03:03 +0700

    13/03/2024

--- QLKTXWEBSITE/Areas/AdminQL/Controllers/OccupanciesController.cs
+++ QLKTXWEBSITE/Areas/AdminQL/Controllers/OccupanciesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -170,5 +172,63 @@ namespace QLKTXWEBSITE.Areas.AdminQL.Controllers
         {
           return (_context.Occupancies?.Any(e => e.OccupancyId == id)).GetValueOrDefault();
         }
+        // POST: AdminQL/Occupancies/SendEmailToOccupants
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SendEmailToOccupants()
+        {
+            try
+            {
+                var occupants = await _context.Occupancies
+                    .Include(o => o.Student)
+                    .ToListAsync();
+
+                foreach (var occupancy in occupants)
+                {
+                    // Gửi email tới địa chỉ email của cư dân
+                    string toEmail = occupancy.Student.Email;
+                    string subject = "Thông báo từ hệ thống quản lý KTX";
+                    string body = "Xin chào " + occupancy.Student.FullName + ",\n\n"
+                                + "Nội dung email: Bạn đang ở trong phòng số " + occupancy.Room.NumberRoom + ".\n\n"
+                                + "Xin vui lòng kiểm tra thông tin đăng nhập tại đây: https://yourwebsite.com/login\n\n"
+                                + "Trân trọng,\n"
+                                + "Hệ thống quản lý KTX";
+
+                    // Thiết lập thông tin của email người gửi
+                    string fromEmail = "21111064572@gmail.com";
+                    string fromPassword = "Danli29.03"; // Nhập mật khẩu ứng với tài khoản Gmail
+                    string host = "smtp.gmail.com";
+                    int port = 587;
+
+                    // Gửi email
+                    var smtpClient = new SmtpClient(host, port)
+                    {
+                        Credentials = new NetworkCredential(fromEmail, fromPassword),
+                        EnableSsl = true
+                    };
+
+                    var mailMessage = new MailMessage
+                    {
+                        From = new MailAddress(fromEmail),
+                        Subject = subject,
+                        Body = body,
+                        IsBodyHtml = true 
+                    };
+
+                    mailMessage.To.Add(toEmail);
+
+                    await smtpClient.SendMailAsync(mailMessage);
+                }
+
+                TempData["SuccessMessage"] = "Email đã được gửi thành công cho tất cả cư dân.";
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = "Đã xảy ra lỗi khi gửi email: " + ex.Message;
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
