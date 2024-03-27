4795c1047e8654fd1c7754e054e68af64a9603f1
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2024-03-27 19:47:49 +0700

    27/03

---
+++ QLKTXWEBSITE/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QLKTXWEBSITE.Controllers
+{
+	public class BaseController : Controller
+	{
+		protected bool IsLoggedIn
+		{
+			get
+			{
+				// Thực hiện logic kiểm tra xem người dùng đã đăng nhập hay chưa
+				// Ví dụ: 
+				return User.Identity.IsAuthenticated;
+			}
+		}
+	}
+}
