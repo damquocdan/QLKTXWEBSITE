e55edd2047c45761a088e24f54334445aaf17fb3
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2024-04-04 23:31:00 +0700

    04/04/2024

--- QLKTXWEBSITE/Areas/AdminQL/Controllers/DashboardController.cs
+++ QLKTXWEBSITE/Areas/AdminQL/Controllers/DashboardController.cs
@@ -1,13 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QLKTXWEBSITE.Models;
+using System.Linq;
 
 namespace QLKTXWEBSITE.Areas.AdminQL.Controllers
 {
-    //[Area("Admins")]
     public class DashboardController : BaseController
     {
+        private readonly QlktxContext _context;
+
+        public DashboardController(QlktxContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var totalStudents = _context.Students.Count();
+            var studentsWithoutBed = _context.Students.Count(s => s.BedId == null);
+            var totalBeds = _context.BedOfRooms.Count();
+            var bedsWithoutStudent = _context.BedOfRooms.Count(b => b.Status == false);
+            var totalFalseServices = _context.Services.Count(s => s.Status == false);
+            var totalServices = _context.Services.Count();
+            var notPayMoney = _context.Occupancies.Count(s=>s.Status == false);
+            var totalPayMoney = _context.Occupancies.Count();
+
+            var viewModel = new DashboardViewModel
+            {
+                TotalStudents = _context.Students.Count(),
+                TotalRooms = _context.Rooms.Count(),
+                TotalBeds = _context.BedOfRooms.Count(),
+                StudentsWithoutBed = _context.Students.Count(s => s.BedId == null),
+
+                BedsWithoutStudent = _context.BedOfRooms.Count(b => b.Status ==false),
+                StudentWithoutBedPercentage = (double)studentsWithoutBed / totalStudents * 100 ,
+                BedWithoutStudentPercentage = (double)bedsWithoutStudent / totalBeds * 100,
+                TotalFalseServices = totalFalseServices,
+                TotalServices = totalServices,
+                TotalFalseServicesPercentage =(double)totalFalseServices / totalServices * 100,
+                NotPaymoney = notPayMoney,
+                TotalPaymoney = totalPayMoney,
+                NotPaymoneyPercentage = (double)notPayMoney /totalPayMoney * 100,
+
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ QLKTXWEBSITE/Models/DashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace QLKTXWEBSITE.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalStudents { get; set; }
+        public int TotalRooms { get; set; }
+        public int TotalBeds { get; set; }
+        public double StudentWithoutBedPercentage { get; set; }
+        public double BedWithoutStudentPercentage {  get; set; }
+        public double TotalFalseServices { get; set; }
+        public double TotalServices { get; set; }
+        public double TotalFalseServicesPercentage { get; set; }
+        public int StudentsWithoutBed { get; set; } // Số sinh viên chưa có giường
+        public int BedsWithoutStudent { get; set; } // Số giường chưa có sinh viên
+        public int NotPaymoney { get; set; }
+        public int TotalPaymoney { get; set;}
+        public double NotPaymoneyPercentage { get; set; }
+    }
+}
