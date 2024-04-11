b9801bd03da1bd688a7f27252cc56256ab57bc13
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2024-04-11 08:03:44 +0700

    11/04/2024

--- QLKTXWEBSITE/Areas/StudentUser/Controllers/DashboardController.cs
+++ QLKTXWEBSITE/Areas/StudentUser/Controllers/DashboardController.cs
@@ -25,5 +25,56 @@ namespace QLKTXWEBSITE.Areas.StudentUser.Controllers
         {
             return View();
         }
+        public IActionResult Service(int? id)
+        {
+            var student = _context.Students
+                .Include(s => s.Bed)
+                .FirstOrDefault(s => s.StudentId == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            if (student.BedId == null)
+            {
+                var roomsInBuildingsD = _context.Rooms
+                .Where(r => r.Building == "D")
+                .ToList();
+
+                var roomsInBuildingsE = _context.Rooms
+                    .Where(r => r.Building == "E")
+                    .ToList();
+
+                var roomsInBuildingsG = _context.Rooms
+                    .Where(r => r.Building == "G")
+                    .ToList();
+
+                // Truyền danh sách phòng của các tòa nhà D, E, G đến view
+                return View("RegisterRoom",new List<List<Room>> { roomsInBuildingsD, roomsInBuildingsE, roomsInBuildingsG });
+
+            }
+            else
+            {
+                // Nếu sinh viên đã có giường, hiển thị form đăng ký các dịch vụ khác
+                return View("RegisterServices");
+            }
+        }
+        public IActionResult GetBeds(int RoomId)
+        {
+            var bedsInRoom = _context.BedOfRooms
+                .Where(b => b.RoomId == RoomId)
+                .ToList();
+
+            return PartialView("_Beds", bedsInRoom);
+        }
+        public IActionResult GetStudents(int BedId)
+        {
+            var studentsInRoom = _context.Students
+                .Where(b => b.BedId == BedId)
+                .ToList();
+
+            return PartialView("_Students", studentsInRoom);
+        }
     }
 }
